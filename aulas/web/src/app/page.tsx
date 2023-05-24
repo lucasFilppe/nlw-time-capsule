dd1287854b4d4465c9e7eb0cb37e6acc0137d5cf
lucasFilppe <lucasfilipe2323@icloud.com>
2023-05-24 01:45:34 -0300

    obetendo github code web

--- aulas/web/src/app/page.tsx
+++ aulas/web/src/app/page.tsx
@@ -15,7 +15,9 @@ export default function Home() {
         <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes pr-2"/>
 
           {/*sign in*/}
-        <a href="" className="flex items-center gap-3 text-left hover:text-gray-50 transaction-colors">
+        <a href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
+          className="flex items-center gap-3 text-left hover:text-gray-50 transaction-colors"
+        >
           <div className='flex h-10 items-center justify-center rounded-full bg-gray-400'>
             <User className="h-5 w-5 text-gray-500"/>
           </div>
